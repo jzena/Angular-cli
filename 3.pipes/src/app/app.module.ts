9fe12a1ed69dc6addf613bf68bf3aa232d73c47b
jose zeña <jose.zena.zamora@gmail.com>
2017-03-01 23:25:16 -0500

    pipes primera parte

---
+++ 3.pipes/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { LOCALE_ID } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: "es" }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
