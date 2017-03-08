2f8dde7d1ef4e14b8f921da3192ed00c5f91ef65
jzena <jose.zena.zamora@gmail.com>
2017-03-08 17:37:56 -0500

    app miscelaneos inicio

---
+++ 6.Miscelaneos/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+import { ClasesComponent } from './components/clases/clases.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 6.Miscelaneos/src/app/components/clases/clases.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html'
+})
+export class ClasesComponent implements OnInit {
+
+  alerta: string = "alert-danger";
+  propiedades: Object = {
+    danger: false
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
