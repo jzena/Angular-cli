6ab2bd0c870738351df2a2ed69018f2842af5299
jzena <jose.zena.zamora@gmail.com>
2017-03-10 17:39:48 -0500

    app formularios finished

--- 8.Formularios/src/app/components/data/data.component.ts
+++ 8.Formularios/src/app/components/data/data.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-data',
@@ -7,9 +10,130 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: "fernando",
+      apellido: "herrera"
+    },
+    correo: "fernando.herrera@gmail.com",
+    //pasatiempos: ["correr", "Dormir", "Comer"]
+  }
+
+  constructor() {
+
+    console.log(this.usuario);
+
+    this.forma = new FormGroup({
+
+      'nombrecompleto': new FormGroup({
+        'nombre': new FormControl('', [
+          Validators.required,
+          Validators.minLength(3)
+        ]),
+        'apellido': new FormControl('', [
+          Validators.required,
+          this.noHerrera
+        ])
+      }),
+      'correo': new FormControl('', [
+        Validators.required,
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+      ]),
+      'pasatiempos': new FormArray([
+        new FormControl('Correr', Validators.required)
+      ]),
+      'username': new FormControl('', Validators.required, this.existeUsuario),
+      'password01': new FormControl('', Validators.required),
+      'password02': new FormControl('', Validators.required)
+    });
+
+    //this.forma.setValue(this.usuario);
+
+    this.forma.controls['password02'].setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma)
+    ])
+
+    this.forma.controls['username'].valueChanges.subscribe(data => {
+      console.log(data);
+    });
+
+    this.forma.controls['username'].statusChanges.subscribe(data => {
+      console.log(data);
+    });
+  }
 
   ngOnInit() {
+  }
+
+  agregarPasatiempo() {
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('Dormir', Validators.required)
+    )
+  }
+
+
+  noHerrera(control: FormControl): { [s: string]: boolean } {
+    if (control.value === "herrera") {
+      return {
+        noherrera: true
+      }
+    }
+
+    return null;
+  }
+
+  noIgual(control: FormControl): { [s: string]: boolean } {
+    //noIgual(control: FormControl): any {
+
+    //console.log(this);
+    let forma: any = this;
+    if (control.value !== forma.controls['password01'].value) {
+      return {
+        noiguales: true
+      }
+    }
+
+    return null;
+  }
+
+  existeUsuario(control: FormControl): Promise<any> | Observable<any> {
+
+    let promesa = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          if (control.value === "strider") {
+            resolve({
+              existe: true
+            })
+          }
+          else {
+            resolve(null)
+          }
+        }, 3000)
+      }
+    )
+
+    return promesa;
+
+  }
+
+  guardarCambios() {
+    console.log(this.forma.value);
+    console.log(this.forma);
+
+    // this.forma.reset({
+    //   nombrecompleto: {
+    //     nombre: "",
+    //     apellido: ""
+    //   },
+    //   correo: ""
+    // });
+
+    //this.forma.controls['correo'].setValue('asdasd@adsasd.com');
+
   }
 
 }
--- 8.Formularios/src/app/components/template/template.component.ts
+++ 8.Formularios/src/app/components/template/template.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+      .ng-invalid.ng-touched:not(form) {
+        border: 1px solid red;
+      }
+  `]
 })
 export class TemplateComponent implements OnInit {
+
+  usuario: Object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais: "",
+    sexo: "Hombre",
+    acetpa: false
+  }
+
+  paises = [{
+    codigo: "CRI",
+    nombre: "Costa Rica"
+  },
+  {
+    codigo: "ESP",
+    nombre: "España"
+  }
+  ];
+
+  sexos: string[] = ["Hombre", "Mujer", "Sin definir"];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  guardar(forma: NgForm) {
+
+    console.log(" ngForm", forma);
+    console.log("valor forma:", forma.value);
+
+    console.log("Usuario", this.usuario);
+  }
 }
